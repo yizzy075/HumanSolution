87af1561ba9b4eed83472b1ca847a2e356ff6ea2
yizzy__ <169167871+yizzy075@users.noreply.github.com>
2025-11-05 20:54:39 -0500

    prueba 2

--- src/main/java/co/edu/uco/HumanSolution/controller/GlobalExceptionHandler.java
+++ src/main/java/co/edu/uco/HumanSolution/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package co.edu.uco.HumanSolution.controller;
 
 import co.edu.uco.HumanSolution.controller.dto.response.ResponseDTO;
+import co.edu.uco.HumanSolution.crosscutting.exception.BusinessException;
 import co.edu.uco.HumanSolution.crosscutting.exception.ControllerException;
 import co.edu.uco.HumanSolution.crosscutting.exception.HumanSolutionException;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ControllerException.class)
     public ResponseEntity<ResponseDTO<Object>> handleControllerException(ControllerException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResponseDTO.builder()
+                        .success(false)
+                        .message(exception.getUserMessage())
+                        .data(null)
+                        .build());
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ResponseDTO<Object>> handleBusinessException(BusinessException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ResponseDTO.builder()
                         .success(false)
